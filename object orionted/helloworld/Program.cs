9dc133dc92c2790a09f5b5f89f090c577c75bc4d
Himaya Sandhamini Perera <hsperera@students.nsbm.ac.lk>
2026-01-27 17:50:13 +0000

    my 1st oop

---
+++ object orionted/helloworld/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Program 1...");
+            // This calls your class in the other file
+           
+        }
+    }
+}
